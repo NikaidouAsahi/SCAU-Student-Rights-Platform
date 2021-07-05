9b3bb047a57c3bbc07d670336bafb8860d68d5fb
Vincent Tsang <vincent_tsang2017@outlook.com>
2021-07-05 18:51:08 +0800

    0.0.1

---
+++ Student-Rights-Platform/src/main/java/com/vincent/studentrightsplatform/Fucker.java
@@ -0,0 +1,14 @@
+package com.vincent.studentrightsplatform;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Fucker {
+    @Autowired
+    @GetMapping({"/index", "/"})
+    public String index() {
+        return "index";
+    }
+}
---
+++ Student-Rights-Platform/src/test/java/com/vincent/studentrightsplatform/StudentRightsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.studentrightsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRightsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
